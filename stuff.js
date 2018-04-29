1234ab76ee321572dd753fca2a55804ec7c83cfc
marulango <hola@maruma.ru>
2018-04-29 13:24:54 -0500

    Uses forEach() instead of plain awful for()

--- stuff.js
+++ stuff.js
@@ -1,7 +1,8 @@
+/* global $ */
 var baseURL = 'https://api.xivdb.com'
 var $target = $('.autocompleteContent')
 
-var $ul = document.createElement('div')
+//var $ul = document.createElement('div')
 
 $(document).on('click', '.remove', function (e) {
   $(e.target).parent().parent().remove()
@@ -23,18 +24,25 @@ $.getJSON(`${baseURL}/recipe`, function (data) {
     select: (event, ui) => {
       $.getJSON(`${baseURL}/recipe/${ui.item.id}`, function (data) {
         var itemName = data.name
-        itemClassName = itemName.replace(/\s+/g, '-').replace(/'/g, '').toLowerCase()
-        console.log(itemClassName)
+        var itemClassName = itemName.replace(/\s+/g, '-').replace(/'/g, '').toLowerCase()
         $target.prepend('<div class="material-item ' + itemClassName + '"></div>')
         $('.' + itemClassName).append('<div class="itemName"><div class="remove"></div><h2>' + itemName + '</h2></div>')
         $('.' + itemClassName).append('<div class="matsHeader row"><div class="col-8"><label>Material</label></div><div class="col-4"><label>QTY</label></div></div>') // this could be a variable…?
 
         var craftableTree = data.tree
-        if (craftableTree.length > 0 == true) {
-          for (var i = 0; i < craftableTree.length; i++) {
-            $('.' + itemClassName).append('<li class="row"><div class="col-8"><p>' + craftableTree[i].name + '</p></div><div class="col-4"><p>' + craftableTree[i].quantity + '</p></div></li>') // this can be much more legible. but idc about that rn
-          }
-        }
+
+        craftableTree.forEach((treeNode) => {
+          var itemName = treeNode.name
+          var itemQty = treeNode.quantity
+          var itemSynths = treeNode.synths || {}
+          console.log(itemName, itemQty)
+          Object.keys(itemSynths).forEach(key => {
+            itemSynths[key].tree.forEach((material) => {
+              var itemMats = material.name
+              console.log(itemMats)
+            })
+          })
+        })
       })
     }
   })
